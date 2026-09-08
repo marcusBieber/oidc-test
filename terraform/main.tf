17fb3622ce2f0531fef4f1e08479d39bc0341861
Marcus <marcus@TPMBIEBER-1.sva.de>
2026-09-08 15:48:23 +0200

    Configure S3 Terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+  
+  backend "s3" {
+    bucket       = "marcus-terraform-state-798836978111"
+    key          = "oidc-test/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  } 
 }
 
 provider "aws" {
