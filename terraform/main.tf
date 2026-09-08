126d5853b9c07146675038529099881a4ee643fc
Marcus <marcus@TPMBIEBER-1.sva.de>
2026-09-08 15:16:12 +0200

    test terraform deployment

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "oidc_test" {
+  bucket_prefix = "marcus-oidc-test-"
+
+  tags = {
+    Name      = "GitHub OIDC Terraform Test"
+    ManagedBy = "Terraform"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.oidc_test.bucket
+}
