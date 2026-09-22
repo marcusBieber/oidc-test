fe48ec0188286ea18fbc0573986bf2bd9dbab7d1
Marcus <marcus@TPMBIEBER-1.sva.de>
2026-09-22 14:27:33 +0200

    add: added account id's

--- terraform/hsr-dev/backend.tf
+++ terraform/hsr-dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # TODO: <AWS_ACCOUNT_ID_DEV> durch die echte Account-ID des hsr-dev-Accounts ersetzen.
-    bucket       = "infra-state-dev-<AWS_ACCOUNT_ID_DEV>"
+    bucket       = "infra-state-dev-021541384408"
     key          = "oidc-test/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
